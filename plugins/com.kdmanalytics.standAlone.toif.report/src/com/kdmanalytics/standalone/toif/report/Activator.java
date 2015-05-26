527c1cdba60f9c6cae498cfb3db6a1d9a4be4c09
Kyle Girard <kyle@kdmanalytics.com>
2015-05-26 13:40:26 -0400

    Add missing standalone report

---
+++ plugins/com.kdmanalytics.standAlone.toif.report/src/com/kdmanalytics/standalone/toif/report/Activator.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2015 KDM Analytics, Inc. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Open Source
+ * Initiative OSI - Open Software License v3.0 which accompanies this
+ * distribution, and is available at http://www.opensource.org/licenses/osl-3.0.php/
+ ******************************************************************************/
+package com.kdmanalytics.standalone.toif.report;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "com.kdmanalytics.standalone.toif.report"; //$NON-NLS-1$
+
+	// The shared instance
+	private static Activator plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+}
